2d589a27dfdfbbff7aaf0534869bddecef6697dd
Zucarita <rodrigo.cistaro@gmail.com>
2022-10-20 02:32:48 -0300

    SignIn Complete

--- src/lib/passport.js
+++ src/lib/passport.js
@@ -1,10 +1,31 @@
-const passport      = require('passport');
+const { request } = require('express');
+const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
+const pool = require('../database');
+const helpers = require('../lib/helpers');
 
-const pool          = require('../database');
-const helpers       = require('../lib/helpers');
+passport.use('local.signin', new LocalStrategy({
+    usernameField: 'username',
+    passwordField: 'password',
+    passReqToCallback: true
+}, async (req, username, password, done) => {
+    console.log(req.body)
+    const rows = await pool.query('SELECT * FROM users WHERE username = ?', [username]);
+    if (rows.length > 0) {
+        const user = rows[0];
+        const validPassword = await helpers.matchPassword(password, user.password);
+        if(validPassword) {
+            done(null, user, req.flash ('success', 'Welcome' + user.username));
+        } else{
+            done(null, false, req.flash('message', 'Incorrect Password'));
+        }
+    }else{
+       return done(null, false, req.flash('message', 'Username does not exist')); 
+    }
+}));
 
-passport.use('local.signup', new LocalStrategy ({
+
+passport.use('local.signup', new LocalStrategy({
     usernameField: 'username',
     passwordField: 'password',
     passReqToCallback: true
@@ -19,9 +40,9 @@ passport.use('local.signup', new LocalStrategy ({
     };
 
     newUser.password = await helpers.encryptPassword(password);
-    const result     = await pool.query('INSERT INTO users SET ?', [newUser] );
-    newUser.id       = result.insertId;
-    return done (null,newUser);
+    const result = await pool.query('INSERT INTO users SET ?', [newUser]);
+    newUser.id = result.insertId;
+    return done(null, newUser);
 
 }));
 
@@ -31,6 +52,6 @@ passport.serializeUser((user, done) => {
 
 
 passport.deserializeUser(async (id, done) => {
-   const rows = await pool.query('SELECT * FROM users WHERE id = ?', [id]);
-   done(null, rows[0]);
+    const rows = await pool.query('SELECT * FROM users WHERE id = ?', [id]);
+    done(null, rows[0])
 });
