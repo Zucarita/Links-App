64977e2c0e096b59b84794fe3a81ac4888d10169
Zucarita <rodrigo.cistaro@gmail.com>
2022-10-25 00:41:43 -0300

    Update, app funcional 100%

--- src/routes/authentication.js
+++ src/routes/authentication.js
@@ -2,22 +2,23 @@ const express  = require('express');
 const router   = express.Router();
 
 const passport = require('passport');
+const { isLoggedIn, isNotLoggedIn } = require ('../lib/auth');
 
-router.get('/signup', (req, res) => {
+router.get('/signup', isNotLoggedIn, (req, res) => {
     res.render ('auth/signup');
 });
 
-router.post('/signup', passport.authenticate ('local.signup',  {
+router.post('/signup', isNotLoggedIn ,passport.authenticate ('local.signup',  {
         successRedirect: '/profile',
         failureRedirect: '/signup',
         failureFlash   : true
     }));
     
-    router.get('/signin', (req, res) => {
+    router.get('/signin',isNotLoggedIn, (req, res) => {
     res.render('auth/signin');
  });
 
- router.post('/signin', (req, res, next) => {
+ router.post('/signin', isNotLoggedIn, (req, res, next) => {
     passport.authenticate('local.signin', {
     successRedirect: '/profile',
     failureRedirect: '/signin',
@@ -25,9 +26,26 @@ router.post('/signup', passport.authenticate ('local.signup',  {
     })(req, res, next);
  });
  
-router.get('/profile', (req, res) => {
-    res.send('this is your profile');
+router.get('/profile', isLoggedIn, (req, res) => {
+    res.render('profile');
 });
+
+
+//CON ESTA FUNCION ME DECIA "LOGOUT FUNCTION REQUIRES A CALLBACK FUNCTION.. PREGUNTAR A MAXI"
+
+// router.get('/logout', (req, res, next) => {
+//     req.logOut();
+//     res.redirect('/signin');
+// });
+
+router.get('/logout',function(req, res, next) {
+    req.logout(function(err) {
+      if (err) { 
+        return next(err); 
+        }
+      res.redirect('/signin');
+    });
+  });
 
 
 module.exports = router;
